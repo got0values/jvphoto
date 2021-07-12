9f383f35f76432386ff96d5aabbd73992eace189
got0values <gotnovalues@gmail.com>
2021-07-12 20:41:14 +0000

    added custom blog page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,12 @@
 // Put your application javascript here
+
+// sort products
+if(document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change', function(e) {
+        // get current url
+        var url = new URL(window.location.href);
+        // add ?sort_by=[value of selected] to the url
+        url.searchParams.set('sort_by', e.currentTarget.value);
+        window.location = url.href;
+    });
+}
